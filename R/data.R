608e7152b504e1d989b17212f3206247b5c4620b
Maclean <I.M.D.Maclean@exeter.ac.uk>
2026-06-03 17:36:48 +0100

    Weather data added

--- R/data.R
+++ R/data.R
@@ -22,3 +22,22 @@
 #' }
 #' @source <https://gmd.copernicus.org/articles/11/2857/2018/gmd-11-2857-2018.pdf>
 "PFTtable"
+
+#' A data frame of hourly weather
+#'
+#' A data frame of hourly weather in 2017 at Caerthillean Cove, Lizard, Cornwall (49.96807N, 5.215668W)
+#'
+#' @format a data frame with the following elements:
+#' \describe{
+#'  \item{obs_time}{POSIXlt object of dates and times}
+#'  \item{temp}{temperature (degrees C)}
+#'  \item{relhum}{relative humidity (percentage)}
+#'  \item{pres}{atmospheric press (kPa)}
+#'  \item{swdown}{Total downward shortwave radiation (W / m^2)}
+#'  \item{difrad}{Total downward diffuse radiation (W / m^2)}
+#'  \item{lwdown}{Total downward longwave radiation (W / m^2)}
+#'  \item{windspeed}{Wind speed (m/s)}
+#'  \item{winddir}{Wind direction (decimal degrees)}
+#'  \item{precip}{precipitation (mm)}
+#' }
+#' "weather"
